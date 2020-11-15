3b441422b2ef4940bb819f3379490367c1d55a04
Noah Gershmel <nggershmel@crimson.ua.edu>
2020-11-15 15:08:11 -0600

    Skeleton for WiFi Setting

---
+++ DiscordBots/set_wifi.py
@@ -0,0 +1,9 @@
+#TODO log IP address and WiFi SSID
+#Notify the user to connect to arduino
+#Once connected the arduino should send a request for Wifi SSID and password
+#Input this info and send it over to the arduino, immediately begin the server
+
+#TODO arduino side
+#Create a WiFi hotspot and server
+#On connection, send request for SSID and password
+#On receive, wait 10 seconds and then attempt to connect (this gives the server time to start up)
